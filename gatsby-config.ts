b2b1f2faf71e78da7c2b4f24538467cc23c23547
Georgi Georgiev <gegeorgiev@vmware.com>
2022-09-12 00:09:48 +0300

    figured out how the damn sitemap plugin works

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,7 @@ const config: GatsbyConfig = {
     title: `mdx-blog-project`,
     description: `Example project for the Gatsby Head API`,
     image: `/mainImg.png`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `http://localhost:9000`,
     twitterUsername: `auratiger`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
@@ -18,6 +18,73 @@ const config: GatsbyConfig = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     `gatsby-remark-images`,
+    {
+      resolve: `gatsby-plugin-advanced-sitemap`,
+      options: {
+        query: `
+         {
+            posts: allSitePage(filter: {path: {regex: "//posts.*/"}}) {
+               edges {
+                  node {
+                     id
+                     slug: path
+                     url: path
+                     pageContext
+                  }
+               }
+            }
+            categories: allSitePage(filter: {path: {regex: "//categories.*/"}}) {
+               edges {
+                  node {
+                     id
+                     slug: path
+                     url: path
+                  }
+               }
+            }
+         }`,
+        mapping: {
+          posts: {
+            sitemap: `posts`,
+            serializer: (edges: any) => {
+              return edges.map(({ node }: any) => {
+                return {
+                  node: {
+                    id: node.id,
+                    slug: node.slug,
+                    url: node.url,
+                    updated_at: node?.pageContext?.date,
+                    cover_image: node?.pageContext?.image,
+                  },
+                }
+              })
+            },
+          },
+          categories: {
+            sitemap: `categories`,
+            serializer: (edges: any) => {
+              return edges.map(({ node }: any) => {
+                return {
+                  node: {
+                    id: node.id,
+                    slug: node.slug,
+                    url: node.url,
+                  },
+                }
+              })
+            },
+          },
+        },
+        exclude: [
+          `/dev-404-page`,
+          `/404`,
+          `/404.html`,
+          `/offline-plugin-app-shell-fallback`,
+        ],
+        createLinkInHead: true,
+        addUncaughtPages: true,
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,30 +5,38 @@ exports.createPages = async ({ graphql, actions }) => {
    const { createPage } = actions
 
    const result = await graphql(`
-    {
-      allMdx {
-        nodes {
-          frontmatter {
-            slug
-          }
-        }
-      }
+     {
+       allMdx {
+         nodes {
+           frontmatter {
+             slug
+             date
+             image {
+               publicURL
+             }
+           }
+         }
+       }
 
-      categories: allMdx {
+       categories: allMdx {
          distinct(field: frontmatter___category)
-      }
-    }
-  `)
+       }
+     }
+   `)
 
-   result.data.allMdx.nodes.forEach(({ frontmatter: { slug } }) => {
-      createPage({
+   result.data.allMdx.nodes.forEach(
+     ({ frontmatter: { slug, date, image } }) => {
+       createPage({
          path: `/posts/${slug}`,
          component: path.resolve(`src/templates/post-template.tsx`),
          context: {
-            slug,
+           slug,
+           date,
+           image: image.publicURL,
          },
-      })
-   })
+       })
+     }
+   )
 
    result.data.categories.distinct.forEach((category) => {
       createPage({
